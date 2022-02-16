50b7d233de5d9644942a333c5839a22a6f16a26b
anialex17 <aleksanyan.anichka17@gmail.com>
2022-02-16 15:41:25 +0400

    16.02.2022 Ani

--- main/views.py
+++ main/views.py
@@ -14,7 +14,6 @@ from .email import send_activate_mail
 from django.db.models import Count, F, Q
 from .forms import *
 from .models import *
-import uuid
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.contrib.auth.tokens import default_token_generator
 from django.utils.encoding import force_bytes
@@ -28,6 +27,7 @@ class GetContextDataMixin(ListView):
         context = super().get_context_data(**kwargs)
         context['register_form'] = RegisterUserForm()
         context['form'] = LoginUserForm()
+        context['basket'] = Basket.objects.filter(customer=None).first()
         return context
 
 
@@ -167,7 +167,7 @@ class SignInView(FormView):
             messages.add_message(self.request, messages.SUCCESS,f'Welcome {user.username}')
         else:
             messages.warning(self.request, 'Login or password are invalid! Please try again!')
-            return redirect('login')
+            return redirect('home')
         return super().form_valid(form)
 
     def get_success_url(self):
@@ -245,6 +245,8 @@ class VerificationView(View):
 
 
 def password_reset_request(request):
+    register_form = RegisterUserForm()
+    form = LoginUserForm()
     if request.method == "POST":
         password_reset_form = PasswordResetForm(request.POST)
         if password_reset_form.is_valid():
@@ -270,6 +272,4 @@ def password_reset_request(request):
                         return HttpResponse('Invalid header found.')
                     return redirect ("/password_reset/done/")
     password_reset_form = PasswordResetForm()
-    register_form = RegisterUserForm()
-    form = LoginUserForm()
     return render(request=request, template_name="main/password_reset.html", context={"password_reset_form":password_reset_form, 'form':form, 'register_form':register_form})
